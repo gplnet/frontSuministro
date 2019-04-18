648d32053b3bc2a19f0db9caa8d0aa1a94e8257b
gplnet <cberm3o@gmail.com>
2019-04-17 23:34:10 -0500

    Se agrego cambios en usuario

--- src/app/_shared/var.constant.ts
+++ src/app/_shared/var.constant.ts
@@ -1,4 +1,4 @@
 export const HOST = 'http://localhost:8095';
 export const TOKEN_NAME = 'access_token';
-export const TOKEN_AUTH_USERNAME = 'cberm3o';
+export const TOKEN_AUTH_USERNAME = 'cbermeo';
 export const TOKEN_AUTH_PASSWORD = 'caba88codex';
